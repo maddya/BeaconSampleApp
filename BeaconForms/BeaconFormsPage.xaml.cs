eb3da237a024b14f73f0ece0c5043476b73c53c9
Madeleine Austin <Maddy@MSEAVIDCHAN.local>
2017-05-19 08:36:15 -0700

    updates

---
+++ BeaconForms/BeaconFormsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Estimotes;
+using Xamarin.Forms;
+
+namespace BeaconForms
+{
+    public partial class BeaconFormsPage : ContentPage
+    {
+        BeaconService beacons;
+
+        bool UserAlerted;
+
+        public BeaconFormsPage()
+        {
+            beacons = new BeaconService();
+            beacons.Initialize();
+            InitializeComponent();
+
+            UserAlerted = false;
+
+
+			EstimoteManager.Instance.Ranged += (sender, beacons) =>
+			{
+                if (!UserAlerted)
+                {
+                    DisplayAlert("You're in that range", null, "cool");
+                    UserAlerted = true;
+                }
+                EstimoteManager.Instance.StopRanging(new BeaconRegion("estimote", "B9407F30-F5F8-466E-AFF9-25556B57FE6D"));
+			};
+        }
+    }
+}
